a514430cce515100c5fcbc9f144b95e7154eeaa0
Murilo Coelho <m.coelhos1212@gmail.com>
2024-02-17 01:22:56 -0300

    feat: create user info command

---
+++ src/discord/commands/user/user.ts
@@ -0,0 +1,43 @@
+import { Command } from "@/discord/base";
+import { settings } from "@/settings";
+import { hexToRgb } from "@magicyan/discord";
+import { ApplicationCommandType, EmbedBuilder, time } from "discord.js";
+
+new Command({
+    name: "user",
+    description: "Suas informações de usuário!",
+    dmPermission: false,
+    type: ApplicationCommandType.ChatInput,
+    async run(interaction){
+      const { user } = interaction;
+
+      const embed = new EmbedBuilder({
+        author: {
+          name: "Informações do Usuário",
+        },
+        title: user.globalName ?? "n/a",
+        thumbnail: {
+          url: user.avatarURL() ?? "n/a"
+        },
+        color: hexToRgb(settings.colors.theme.info),
+        fields: [
+          {
+            name: "Id",
+            value: user.id,
+            inline
+          },
+          {
+            name: "Tag",
+            value: user.username,
+            inline
+          },
+          {
+            name: "Perfil criado em:",
+            value: `${time(user.createdAt, "D")}, às ${time(user.createdAt, "T")} (${time(user.createdAt, "R")})`
+          }
+        ]
+      });
+
+      interaction.reply({ ephemeral, embeds: [embed] });
+    }
+});
